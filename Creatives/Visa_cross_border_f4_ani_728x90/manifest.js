1fc7561aa7c2610cda20a998db599ddc050af9f0
mohasin-sebpo <mohammed.mohasin@sebpo.com>
2023-11-24 10:38:20 +0600

    728x90
    728x90 creatives

---
+++ Creatives/Visa_cross_border_f4_ani_728x90/manifest.js
@@ -0,0 +1,34 @@
+FT.manifest({
+	"filename": "index.html",
+	"width": 728,
+	"height": 90,
+	"clickTagCount": 1,
+	"hideBrowsers": ["ie8"],
+	"richLoads": [
+		{ "name": "f4_animation", "src": "VISA_AU_728x90_hands_RL" }
+	],
+	"instantAds": [
+		{ "name": "f4_animation", "type": "richLoad" },
+		{ "name": "frame1_headline", "type": "text", "default": "Unlock more of Japan with Visa." },
+		{ "name": "frame1_subheadline", "type": "text", "default": "Save 20% off participating IHG® Hotels & Resorts." },
+		{ "name": "frame_2_headline", "type": "text", "default": "From crusing<br>the slopes" },
+		{ "name": "frame_3_headline", "type": "text", "default": "to finding<br>stillness" },
+		{ "name": "frame_4_headline", "type": "text", "default": "Pay securely with Visa wherever you go." },
+		{ "name": "endframe_headline", "type": "text", "default": "Accepted at over 100 million locations worldwide." },
+		{ "name": "endframe_cta", "type": "text", "default": "Learn More" },
+		{ "name": "endframe_background_color", "type": "text", "default": "#1434cb" },
+		{ "name": "background_image_01", "type": "image", "default": "images/f1-2_728x90_Japan.jpg" },
+		{ "name": "background_image_03", "type": "image", "default": "images/f3_728x90_Japan.jpg" },
+		{ "name": "overlay_image", "type": "image", "default": "images/gradient_728x90.png" },
+		{ "name": "logo1", "type": "image", "default": "images/VISA_logo_wh.svg" },
+		{ "name": "logo2", "type": "image", "default": "images/blank.png" },
+		{ "name": "endframe_logo", "type": "image", "default": "images/logo_lockup.svg" },
+		{ "name": "animation_2frames", "type": "text", "default": "off" },
+		{ "name": "dynamic_clickthrough", "type": "text", "default": "cta" },
+		{ "name": "frame1_headline_color", "type": "text", "default": "#000" },
+		{ "name": "frame2_headline_color", "type": "text", "default": "#1434cb" },
+		{ "name": "frame3_headline_color", "type": "text", "default": "#fff" },
+		{ "name": "frame4_headline_color", "type": "text", "default": "#1434cb" },
+		{ "name": "endframe_headline_color", "type": "text", "default": "#fff" }
+	]
+});
